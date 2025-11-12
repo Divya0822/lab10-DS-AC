4acfd96248f96c999900e7d7074ca64fc6ca3fc7
Divya0822 <b24429@students.iitmandi.ac.in>
2025-11-12 15:57:31 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,11 +1,13 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
+# https://github.com/Divya0822/COP-Lab
+# Partner 1: Divya Singh
+# Partner 2: Anabel
 import math
+def square_root(a):
+    if a<0:
+        raise ValueError("")
+    return math.sqrt(a)
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 def add(a, b):
     return a+b 
 def sub(a,b):
@@ -22,6 +24,7 @@ def log(a, b):
     return math.log(b,a)
 def exp(a, b):
     return a**b
+
 
 
 
