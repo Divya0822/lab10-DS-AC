736358fd5a11cdd56364faf247f1d36f5fa6da1f
anabch20 <anabelcchavez10@gmail.com>
2025-11-12 15:46:33 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -7,15 +7,19 @@ One function per operation, in order.
 # First example
 import math
 def add(a, b):
-    return a+b 
+    return a+b
+
 def sub(a,b):
     return a-b
+
 def mul(a, b):
     return a*b
+
 def div(a, b):
     if a == 0:
-        raise ZeroDivisionError("")
+        raise ZeroDivisionError("Division Error. Cannot divide by zero")
     return b/a
+
 def log(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("")
